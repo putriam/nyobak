f71582fbaf9726a66d8261cdef5a714d86c17833
Putri Amelia <49051097+putriam@users.noreply.github.com>
2020-04-25 19:16:30 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author YACOB
+ */
+public class Mahasiswa {
+    private String nim;
+    private String namamhs;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNamamhs() {
+        return namamhs;
+    }
+
+    public void setNamamhs(String namamhs) {
+        this.namamhs = namamhs;
+    }
+    
+   
+}
